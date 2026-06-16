64a52d5a1aad6ac5293b817a18c6b73a5dc857f9
Yiğit Emre Yıldırım <yldrmyigit00@gmail.com>
2026-06-16 23:09:09 +0300

    Update nyxa_mac_changer.py

--- nyxa_mac_changer.py
+++ nyxa_mac_changer.py
@@ -1,10 +1,18 @@
+# you must be root
 import subprocess
 
 print("Nyxa Mac Changer is starting")
 
-subprocess.call(["ifconfig","eth0","down"])
-subprocess.call(["ifconfig","eth0","hw","ether","00:11:22:33:44:55"])
-subprocess.call(["ifconfig","eth0","up"])
+interface = "eth0"
+mac_address = "00:11:22:33:44:55"
+
+
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether",mac_address])
+subprocess.call(["ifconfig",interface,"up"])
+
+subprocess.call(["ifconfig",interface])
+
 
 print("Mac Address Changed Successfully")
 
