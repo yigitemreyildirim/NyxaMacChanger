15309bcdae5e62de3ca39d573d71e3115a4843a2
Yiğit Emre Yıldırım <yldrmyigit00@gmail.com>
2026-06-17 10:16:33 +0300

    Update nyxa_mac_changer.py

--- nyxa_mac_changer.py
+++ nyxa_mac_changer.py
@@ -1,17 +1,29 @@
 # you must be root
 import subprocess
+import optparse
 
-print("Nyxa Mac Changer is starting")
+print("Nyxa Mac Changer Started")
+
+parse_object = optparse.OptionParser()
+
+parse_object.add_option("-i","--interface",dest="interface",default="eth0",help="Interface to change mac address")
+parse_object.add_option("-m","--mac",dest="mac_address",default="eth0",help="new mac address")
+
+(user_input,arguments) = parse_object.parse_args()
+
+user_interface = user_input.interface
+user_mac_address = user_input.mac_address
 
 interface = "eth0"
 mac_address = "00:11:22:33:44:55"
 
+subprocess.call(["ifconfig",user_interface,"down"])
+subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_address])
+subprocess.call(["ifconfig",user_interface,"up"])
 
-subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig",interface,"hw","ether",mac_address])
-subprocess.call(["ifconfig",interface,"up"])
-
-subprocess.call(["ifconfig",interface])
+subprocess.call(["clear"])
+print(user_interface)
+print(user_mac_address)
 
 
 print("Mac Address Changed Successfully")
